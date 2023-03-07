5bc58890bc7dba78ed55979d7a4322a01ee1e42a
Arpit Sahu <aptsahu2@gmail.com>
2023-03-07 19:38:21 +0530

    Added switch Statement

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,30 @@ public class Main {
         double b = sc.nextDouble();
         System.out.println("Enter your choice \n 1) Addition \n 2) Subtraction \n 3) Multiplication \n 4) Division \n 5) Remainder");
         int choice = sc.nextInt();
-        if(choice == 1){
-            double add = a + b;
-            System.out.println("The addition is  "+add);
-        } else if (choice == 2) {
-            double sub = a - b;
-            System.out.println("The subtraction is  "+sub);
-        } else if (choice == 3) {
-            double mul = a * b;
-            System.out.println("The multiplication is  "+mul);
-        } else if (choice == 4 ) {
-            double div = a / b;
-            System.out.println("The division is  "+div);
-        } else if (choice == 5) {
-            double mod = a % b;
-            System.out.println("The remainder is  "+mod);
-        } else{
-            System.out.println("Wrong Choice");
+        switch (choice){
+            case 1:
+                double add = a + b;
+                System.out.println("The addition is  "+add);
+                break;
+            case 2 :
+                double sub = a - b;
+                System.out.println("The subtraction is  "+sub);
+                break;
+            case 3 :
+                double mul = a * b;
+                System.out.println("The multiplication is  "+mul);
+                break;
+            case 4:
+                double div = a / b;
+                System.out.println("The division is  "+div);
+                break;
+            case 5:
+                double mod = a % b;
+                System.out.println("The remainder is  "+mod);
+                break;
+            default:
+                System.out.println("Wrong Choice");
+                break;
         }
         sc.close();
     }
