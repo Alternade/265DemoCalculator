b1fa23a75854670571082cd52e2ab3e0a580a7e4
Arpit Sahu <aptsahu2@gmail.com>
2023-03-08 20:01:45 +0530

    Added methods

---
+++ src/demo1.java
@@ -0,0 +1,12 @@
+public class demo1 {
+    public static void main(String[] args) {
+        int i=1,j = 0;
+        while ( i < 10 && j< 5){
+            System.out.println(i);
+            System.out.println(j);
+            i++;j++;
+       }
+
+
+    }
+}
